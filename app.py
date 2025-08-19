7393e9468e736470e767932cb1ed87a48fa49749
gerardoGP <gerardo210299@gmail.com>
2025-08-19 15:27:03 -0500

    final

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from flask_cors import CORS
+from .routes import bp
+
 app = Flask(__name__)
+# Añadimos una clave secreta para habilitar las sesiones de Flask
+app.secret_key = 'super-secret-key-for-demo' # En producción, esto debería ser un valor seguro y aleatorio
+CORS(app, supports_credentials=True) # supports_credentials=True es necesario para las sesiones
 
-@app.route("/")
-def hello_world():
-    return "<p>Hola, mundo!</p>"
+app.register_blueprint(bp)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run()
