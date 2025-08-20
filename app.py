7032517038be8b3447f25b96818e3eef4df04afa
gerardoGP <gerardo210299@gmail.com>
2025-08-19 21:31:28 -0500

    cors

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # from flask import Flask
-# from flask_cors import CORS
 # from .routes import bp
+from flask_cors import CORS
 import random
 from flask import Flask, jsonify, request, session
 from game import (
@@ -11,8 +11,8 @@ from game import (
 )
 app = Flask(__name__)
 # Añadimos una clave secreta para habilitar las sesiones de Flask
-# app.secret_key = 'super-secret-key-for-demo' # En producción, esto debería ser un valor seguro y aleatorio
-# CORS(app, supports_credentials=True) # supports_credentials=True es necesario para las sesiones
+app.secret_key = 'super-secret-key-for-demo' # En producción, esto debería ser un valor seguro y aleatorio
+CORS(app, supports_credentials=True) # supports_credentials=True es necesario para las sesiones
 # app.register_blueprint(bp)
 
 @app.route('/', methods=['GET'])
